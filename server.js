7b7f04228eec954c89f6a4603a9ca01f165ce362
Shaitan5723 <105050107+Shaitan5723@users.noreply.github.com>
2022-08-08 18:02:23 +1000

    start menu
    list of choices when function is first run

--- server.js
+++ server.js
@@ -11,5 +11,64 @@ const db = mysql.createConnection({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+},
+console.log(`Connected to the ${process.env.DB_NAME} database.`)
+);
+
+db.connect(function(err) {
+  if (err) throw err;
+  start();
 });
 
+// Main menu that prompts user for instructions
+function start() {
+  inquirer.prompt({
+    name: "start_menu",
+    type: "list",
+    message: "What would you like to do?",
+    choices: ["View all departments", 
+      "View all roles", 
+      "View all employees", 
+      "Add a department",
+      "Add a role", 
+      "Add an employee", 
+      "Update employee role", 
+      "Exit"]
+  })
+      // switch statement used to cycle through reponses based on user selection 
+    .then((res) => {
+      switch (res.start_menu) {
+        case "View all departments":
+          viewDepartments();
+          break;
+
+        case "View all roles":
+          viewRoles();
+          break;  
+
+        case 'View all employees':
+          viewEmployees();  
+          break;
+
+        case "Add a department":
+          addDepartment();
+          break;
+
+        case "Add a role":
+          addRole();
+          break;
+
+        case "Add an employee":
+          addEmployee();
+          break;
+
+        case "Update employee role":
+          updateEmployee();
+          break;
+
+        case "Exit":
+          db.end();
+          break;
+        }
+    });
+};
