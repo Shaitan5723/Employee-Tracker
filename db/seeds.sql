424497b665ed591b2106a73201fc22a57e856554
Shaitan5723 <105050107+Shaitan5723@users.noreply.github.com>
2022-08-08 11:49:50 +1000

    database creation
    added tables and values for db files

---
+++ db/seeds.sql
@@ -0,0 +1,20 @@
+INSERT INTO departments (department_name)
+VALUES ("Customer Service"),
+      ("Sales"),
+      ("R&D");
+
+INSERT INTO roles (title, salary)
+VALUES ("Customer Service Representative", 50000),
+      ("Customer Service Manager", 80000),
+      ("Sales Representative", 55000),
+      ("Sales Manager", 80000),
+      ("R&D Researcher", 70000),
+      ("R&D Manager", 90000);
+
+INSERT INTO employees (first_name, last_name)
+VALUES ("Bob", "Newby"),
+      ("Jim", "Hopper"),
+      ("Will", "Byers"),
+      ("Max", "Mayfield"),
+      ("Conan", "O'Brien"),
+      ("Jordan", "Schlansky"),
