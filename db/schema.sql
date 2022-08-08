ad4dbc9b9d837b54d323d53a0dbe3b21a571d308
Shaitan5723 <105050107+Shaitan5723@users.noreply.github.com>
2022-08-08 12:08:29 +1000

    password protection
    npm dotenv package for password protection
    info loaded into server.js

--- db/schema.sql
+++ db/schema.sql
@@ -5,8 +5,8 @@ USE employees_db;
 
 CREATE TABLE departments(
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  department_name VARCHAR(30) NOT NULL,
-)
+  department_name VARCHAR(30) NOT NULL
+);
 
 CREATE TABLE roles(
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
@@ -15,8 +15,8 @@ CREATE TABLE roles(
   deparment_id INT,
   FOREIGN KEY (department_id)
   REFERENCES deparments(id)
-  ON DELETE SET NULL,
-)
+  ON DELETE SET NULL
+);
 
 CREATE TABLE employees(
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
@@ -29,5 +29,5 @@ CREATE TABLE employees(
   ON DELETE SET NULL,
   FOREIGN KEY (manager_id)
   REFERENCES employees(id)
-  ON DELETE SET NULL,
-)
+  ON DELETE SET NULL
+);
--- server.js
+++ server.js
@@ -1,2 +1,15 @@
 const mysql = require('mysql2');
 const cTable = require('console.table');
+const inquirer = require('inquirer');
+
+require('dotenv').config();
+
+// Create connection to database
+const db = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+});
+
